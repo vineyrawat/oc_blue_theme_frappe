f34b5ef9f402a1a5b100b41ce889f3c223bbd052
frappe <frappe@6e2c5d7e6545>
2022-06-09 12:42:00 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Oc Blue Theme
+
+A Simple blue theme for frappe
+
+#### License
+
+MIT
---
+++ oc_blue_theme/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Oc Blue Theme",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Oc Blue Theme")
+		}
+	]
